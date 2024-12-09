b389f7c2709435e89d73afa960720cffe1251349
kaimon0 <luandasarah1031@gmail.com>
2024-12-09 15:45:36 +0100

    [ADD] maintitle

--- portfolio_app/src/app/Components/MainTitleContainer.js
+++ portfolio_app/src/app/Components/MainTitleContainer.js
@@ -2,8 +2,8 @@ export default function MainTitleContainer() {
 
 
     return (
-        <div className="col-start-1 col-end-6 row-start-1 row-end-2">
-          <h1>MainTitle</h1>
+        <div id="mainTitleContainer" className=" rounded-3xl bg-secondaryColor col-start-1 col-end-6 row-start-1 row-end-2 ">
+          <h1 >MainTitle</h1>
          </div>
        
     )
