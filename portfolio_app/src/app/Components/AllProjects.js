6061332a326b627650434e09adaaa280999400a9
kaimon0 <luandasarah1031@gmail.com>
2025-03-24 17:51:56 +0100

    feat(projects) : [UPDATE] On going

--- portfolio_app/src/app/Components/AllProjects.js
+++ portfolio_app/src/app/Components/AllProjects.js
@@ -5,34 +5,37 @@ const AllProjects = [
     projectId: "puissance-4",
     description:
       "Je me suis amusée à refaire le jeu du puissance 4 en Javascript (programmation orienté objet), HTML et CSS.",
+    solution:"lorem ipsmum dolor sit amet",
     time: "1 semaine",
     technologies: ["JS", "HTML", "CSS"],
     gif: "/gif/puissance4.gif",
     // gallery: ["/thumbnails/puissance4-thumbnail.webp", ],
     link: "",
-    githubLink: "",
+    githubLink: "/",
   },
 
   {
     title: "Spotify Clone",
     projectId: "spotify-clone",
     description: "Petit clone de Spotify réalisé en React.",
+    solution:"lorem ipsmum dolor sit amet",
     time: "3 jours",
     technologies: ["REACT", "CSS", "DOCKER"],
     gif: "/gif/Spotify.gif",
     link: "",
-    githubLink: "",
+    githubLink: "/",
   },
 
   {
     title: "To Do List",
     projectId: "to-do-list",
     description: "Mini to do list réalisée en Javascript.",
+    solution:"lorem ipsmum dolor sit amet",
     time: "4h",
     technologies: ["JS", "CSS", "HTML"],
     gif: "/gif/ToDoList.gif",
     link: "",
-    githubLink: "",
+    githubLink: "/",
   },
 ];
 
--- portfolio_app/src/app/layout.js
+++ portfolio_app/src/app/layout.js
@@ -11,36 +11,36 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap');
+          @import
+          url('https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap');
         </style>
-        {/* <script src="https://kit.fontawesome.com/5c9555d31b.js" crossOrigin="anonymous"></script> */}
+       <link rel="shorcut icon" href="/favicon.ico" />
+  
+      
       </head>
-      <body className=" bg-tertiaryColor lg:bg-bgColor w-screen h-screen " >
+      <body className=" bg-tertiaryColor lg:bg-bgColor w-screen h-screen ">
         <main className=" lg:px-4  w-full h-full grid lg:grid-cols-10 lg:grid-rows-8 md:gap-x-4 overflow-x-hidden">
-        <Navbar />
-        
-        {children}
+          <Navbar />
 
-        <Link 
-        href="https://github.com/luandaSarah" 
-        target="_blank"
-        className="  sticky bottom-5 left-2 z-10   lg:flex lg:flex-col lg:justify-self-end lg:self-center lg:col-start-10  lg:col-end-11 lg:row-start-8 lg:row-end-9 lg:static"
-        >
-        
-        <Image
-                      loading="lazy"
-                      id="meLogo"
-                      src={"/icones/github.webp"}
-                      width={40}
-                      height={40}
-                      alt="logo a propos"
-                     
-                    ></Image>
-        </Link>
+          {children}
+
+          <Link
+            href="https://github.com/luandaSarah"
+            target="_blank"
+            className="  sticky bottom-5 left-2 z-10   lg:flex lg:flex-col lg:justify-self-end lg:self-center lg:col-start-10  lg:col-end-11 lg:row-start-8 lg:row-end-9 lg:static"
+          >
+            <Image
+              loading="lazy"
+              id="meLogo"
+              src={"/icones/github.webp"}
+              width={40}
+              height={40}
+              alt="logo a propos"
+            ></Image>
+          </Link>
         </main>
-        
       </body>
     </html>
   );
