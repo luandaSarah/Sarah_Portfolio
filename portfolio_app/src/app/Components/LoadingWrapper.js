b090b33fa3df2d043521d73580c446ee3586c946
kaimon0 <luandasarah1031@gmail.com>
2025-03-25 19:18:30 +0100

    feat(loader):[ADD] global Loader when first rendering

---
+++ portfolio_app/src/app/Components/LoadingWrapper.js
@@ -0,0 +1,18 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
+
+export default function LoadingWrapper({ content: content }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadApp = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1500)); // Simule un délai de chargement
+      setIsLoading(false);
+    };
+
+    loadApp();
+  }, []);
+
+  return isLoading ? <Loader /> : content;
+}
--- portfolio_app/src/app/layout.js
+++ portfolio_app/src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "./Components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
+import LoadingWrapper from "./Components/LoadingWrapper";
 export const metadata = {
   title: "Sarah's Portfolio",
   description: "Welcome to my portfolio",
@@ -17,28 +18,32 @@ export default function RootLayout({ children }) {
           url('https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap');
         </style>
         <link rel="shorcut icon" href="/favicon.ico" />
-        <script src="https://cdn.jsdelivr.net/npm/iconify-icon@2.3.0/dist/iconify-icon.min.js"></script>
       </head>
       <body className=" bg-tertiaryColor lg:bg-bgColor w-screen h-screen ">
-        <main className=" lg:px-4  w-full h-full grid lg:grid-cols-10 lg:grid-rows-8 md:gap-x-4 overflow-x-hidden">
-          <Navbar />
+        <LoadingWrapper
+          content= {
+            <main className=" lg:px-4  w-full h-full grid lg:grid-cols-10 lg:grid-rows-8 md:gap-x-4 overflow-x-hidden">
+              <Navbar />
 
-          {children}
+              {children}
 
-          <Link
-            href="https://github.com/luandaSarah"
-            target="_blank"
-            className="  sticky bottom-5 left-2 z-10   lg:flex lg:flex-col lg:justify-self-end lg:self-center lg:col-start-10  lg:col-end-11 lg:row-start-8 lg:row-end-9 lg:static"
-          >
-            <Image
-              loading="lazy"
-              src={"/icones/github.webp"}
-              width={40}
-              height={40}
-              alt="logo GitHub"
-            ></Image>
-          </Link>
-        </main>
+              <Link
+                href="https://github.com/luandaSarah"
+                target="_blank"
+                className="  sticky bottom-5 left-2 z-10   lg:flex lg:flex-col lg:justify-self-end lg:self-center lg:col-start-10  lg:col-end-11 lg:row-start-8 lg:row-end-9 lg:static"
+              >
+                <Image
+                  loading="lazy"
+                  id="meLogo"
+                  src={"/icones/github.webp"}
+                  width={40}
+                  height={40}
+                  alt="logo a propos"
+                ></Image>
+              </Link>
+            </main>
+          }
+        />
       </body>
     </html>
   );
