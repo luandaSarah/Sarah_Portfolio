807449ff525091a74ce4db133406b84e3bfe1b17
kaimon0 <luandasarah1031@gmail.com>
2025-03-30 21:03:46 +0200

    feat(project) [ADD] ajout d'une fonctionnalité de scroll into view au clique sur un des projets de la liste

--- portfolio_app/src/app/Components/ProjectsThumbnails.js
+++ portfolio_app/src/app/Components/ProjectsThumbnails.js
@@ -12,26 +12,20 @@ export default function ProjectsThumbnails({
   projectId: projectId,
   projectDesc: projectDesc,
 }) {
-  const projectRef = useRef(null);
-
-  // const getTopOffset = () => {
-  //   // const { offsetTop } = projectRef.current;
-  //   // return offsetTop;
-  // }
+ 
   const handleStacks = (stack) => {
     const foundStack = AllStacks.find((tech) => tech.name === stack);
     return foundStack?.icon();
   };
 
-  // console.error(projectRef);
+
   return (
     <>
       <div
-        className="project-thumbnail-container  pixel-thumbnails  bg-bgColor  mr-4 py-10 px-6  mb-10 min-h-2/3"
-        // ref={projectRef}
-        // onScroll = {getTopOffset}
+        className={ projectId + " project-thumbnail-container  pixel-thumbnails  bg-bgColor  mr-4 py-10 px-6  mb-10 min-h-2/3"}
+        
       >
-        <div className="flex mb-4 gap-5">
+        <div className="flex flex-col  items-center md:items-start md:flex-row mb-4 gap-1 md:gap-5">
           <h2 className="text-xl font-bold text-blackColor ">
             {projectname}
           </h2>
@@ -43,7 +37,7 @@ export default function ProjectsThumbnails({
           </Link>
         </div>
 
-        <div className="w-full gap-x-4 grid grid-cols-5 grid-rows-1">
+        <div className="w-full gap-x-4 md:grid md:grid-cols-5 md:grid-rows-1">
           <div className="pixel-thumbnails col-start-1 col-end-3 h-52 relative  ">
             <Image
               loading="lazy"
@@ -55,13 +49,13 @@ export default function ProjectsThumbnails({
             ></Image>
           </div>
 
-          <div className="col-start-3 col-end-6 row-start-1">
+          <div className="col-start-3 col-end-6 row-start-1 ">
             <div>
-              <p>{projectDesc}</p>
+              <p className="text-center md:text-left">{projectDesc}</p>
               
               {/* <p>{projectSolution}</p> */}
             </div>
-            <div className="tech-grid  md:w-full flex  flex-wrap h-10 col-start-3 col-end-6 ">
+            <div className="tech-grid  md:w-full flex justify-center md:justify-start flex-wrap h-10 col-start-3 col-end-6 ">
               {technologies.map((tech) => (
                 <div key={tech} className="text-center mt-3">
                   {handleStacks(tech)}
